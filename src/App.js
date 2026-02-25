92356600a1fb51a79ad8e82ad42d6980060cd3b7
upendrauv <upendrapailee1626@gmail.com>
2026-02-25 11:26:49 +0530

    Fixed eslint unused import error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import axios from "axios"
-import { useState } from "react"
+
 import { BrowserRouter, Routes,Route } from "react-router-dom"
 import Reg from "./Reg"
 import Home from "./Home"
--- src/Reg.jsx
+++ src/Reg.jsx
@@ -17,12 +17,17 @@ function Reg()
    {
       try
       {
-         const res=await axios.post("http://localhost:8080/reg",data)
+        //  const res=await axios.post("http://localhost:8080/reg",data)
+        const res = await axios.post(
+        "https://cabsystemsms-1.onrender.com/register",
+        data
+      )
          alert(res.data)
       } 
       catch(xyz)
       {
-         alert(xyz.response.data)
+        //  alert(xyz.response.data)
+         alert(xyz.response?.data || "Error")
       }
    }
     return(
