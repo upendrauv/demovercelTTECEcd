f8f1393170a8ef65d3eb9f163f3a949a268a7d3a
upendrauv <upendrapailee1626@gmail.com>
2026-02-25 10:31:55 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios"
+import { useState } from "react"
+import { BrowserRouter, Routes,Route } from "react-router-dom"
+import Reg from "./Reg"
+import Home from "./Home"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App()
+{
+    return(
+      <>
+         <h1>Iam App</h1>
+         <BrowserRouter>
+           <Routes>
+              <Route path="/reg" element={<Reg/>}/>
+              <Route path="/h" element={<Home/>}/>
+           </Routes>
+         </BrowserRouter>
+      </>
+    )
 }
-
-export default App;
+export default App
---
+++ src/Reg.jsx
@@ -0,0 +1,38 @@
+import axios from "axios"
+import { useState } from "react"
+function Reg()
+{
+   const[data,setdata]=useState(
+    {
+      username:"",
+      email:"",
+      password:""
+    }
+   )
+   const changeName=(e)=>
+   {
+      setdata({...data,[e.target.name]:e.target.value})
+   }
+   const submit= async()=>
+   {
+      try
+      {
+         const res=await axios.post("http://localhost:8080/reg",data)
+         alert(res.data)
+      } 
+      catch(xyz)
+      {
+         alert(xyz.response.data)
+      }
+   }
+    return(
+      <>
+         <h1>iam App</h1>
+          <input onChange={changeName} name="username" placeholder="enter username"/>
+          <input onChange={changeName} name="email" placeholder="enter email"/>
+          <input onChange={changeName} name="password" placeholder="enter password"/>
+          <button onClick={submit}>register</button>
+      </>
+    )
+}
+export default Reg
